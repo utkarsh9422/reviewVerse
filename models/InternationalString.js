90ee486d23ae2a2e3112ec76f44e318f5d8c720f
Navneet Kumar <navkumar258@gmail.com>
2016-07-28 12:47:31 +0530

    Delete InternationalString.js

--- models/InternationalString.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var InternationalStringSchema = new mongoose.Schema({
- lang: String,
-  text: String
-});
-
-mongoose.model('InternationalStringSchema', InternationalStringSchema);
