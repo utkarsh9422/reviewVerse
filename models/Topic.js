c39d0d5ea15061db45c34c74c6eee6802d18ef4d
Navneet Kumar <navkumar258@gmail.com>
2016-07-28 12:46:25 +0530

    Update Topic.js

--- models/Topic.js
+++ models/Topic.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 
 var TopicSchema = new mongoose.Schema({
   creation: { type: Date, default: Date.now },
-  name: [InternationalStringSchema],
-  description: [InternationalStringSchema],
+  name: String,
+  description: String,
   upvotes: {type: Number, default: 0},
   avgRating:{type: Number, default: 0},
   parentCategoryId:{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }
