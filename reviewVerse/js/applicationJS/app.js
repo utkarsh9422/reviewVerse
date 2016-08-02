55709126728655f3a5a924bb337b376562b5e0ab
AnuragParihar <anuragparihar.apy@gmail.com>
2016-08-03 00:14:29 +0530

    Update app.js

--- reviewVerse/js/applicationJS/app.js
+++ reviewVerse/js/applicationJS/app.js
@@ -6,56 +6,17 @@ var app = angular.module('gascoApp', ['ngRoute', 'ngMessages', 'pascalprecht.tra
 app.config(['$routeProvider',
 function($routeProvider, $activityIndicatorProvider) {
 
-	$routeProvider.when('/login', {// for first login
-		templateUrl : 'views/PRTL_login.html',
-		controller : 'loginController'
-	});
 	$routeProvider.when('/restaurant', {
 		templateUrl : 'views/PRTL_restaurant.html',
 		controller : 'restaurantController'
 	});
-	$routeProvider.when('/register', {
-		templateUrl : 'views/PRTL_register.html',
-		controller : 'registerController'
-	});
-	$routeProvider.when('/changePassword', {
-		templateUrl : 'views/PRTL_changePassword.html',
-		controller : 'changePasswordController'
+	$routeProvider.when('/login', {// for first login
+		templateUrl : 'views/PRTL_login.html',
+		controller : 'loginController'
 	});
 	$routeProvider.otherwise({
-		redirectTo : '/login'
+		redirectTo : '/restaurant'
 	});
-
-}]);
-
-app.config(['$translateProvider',
-function($translateProvider) {
-
-	$translateProvider.useStaticFilesLoader({
-		prefix : 'translations/',
-		suffix : '.json'
-	}).preferredLanguage('en').useMissingTranslationHandlerLog();
-
-}]);
-
-app.run(['$rootScope', 'sharedService', '$location',
-function($rootScope, sharedService, $location) {
-	$rootScope.lang = 'en';
-
-	$rootScope.default_float = 'right';
-	$rootScope.opposite_float = 'left';
-
-	$rootScope.default_direction = 'rtl';
-	$rootScope.opposite_direction = 'ltr';
-
-	if (sharedService.isLoggedIn()) {//for logged in user the default page is dashboard
-		console.log('DENY');
-		
-		$location.path('/dashboard');
-	} else {
-		console.log('ALLOW');
-		$location.path('/profileSelection');
-	}
 
 }]);
 
