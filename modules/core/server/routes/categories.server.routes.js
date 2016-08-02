cb7fdefbb36f6188dc4853ba8803d8e9c7b810b6
Navneet Kumar <navkumar258@gmail.com>
2016-08-02 11:32:16 +0530

    Added CORS functionality.

--- modules/core/server/routes/categories.server.routes.js
+++ modules/core/server/routes/categories.server.routes.js
@@ -1,9 +1,11 @@
 /* eslint-disable */
 'use strict';
+var cors = require('cors');
 
 module.exports = function(app) {
   // Routing logic   
   // ...
+  app.use(cors());
   var categories = require('../controllers/categories.server.controller');
 var topics = require('../controllers/topics.server.controller');
  var reviews = require('../controllers/reviews.server.controller');
