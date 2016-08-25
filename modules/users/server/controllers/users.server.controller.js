1466a02e41ef08ddf21140fb413554c54e1913ed
utkarsh9422 <UM00427537@techmahindra.com>
2016-08-26 02:27:20 +0530

    console logs

--- modules/users/server/controllers/users.server.controller.js
+++ modules/users/server/controllers/users.server.controller.js
@@ -97,12 +97,13 @@ exports.login = function(req, res) {
 		});
 			}
 			else{
-				console.log(user._id);
+				
 				var data = {
 					_id: user._id,
 					name: user.local.name,
 					email: user.local.email
 				};
+				console.log(data);
 				// all is well, return successful user
 				var token = jwt.sign(data, cfg.sessionSecret, {
 				expiresInMinutes: 5 // expires in 24 hours
