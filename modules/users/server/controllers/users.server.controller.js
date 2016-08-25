f9094f15a2e6db8391b47ac9ad9329d8fc53c842
utkarsh9422 <UM00427537@techmahindra.com>
2016-08-26 00:05:31 +0530

    Authentication Commit

--- modules/users/server/controllers/users.server.controller.js
+++ modules/users/server/controllers/users.server.controller.js
@@ -4,19 +4,111 @@
 /**
  * Module dependencies.
  */
-var _ = require('lodash');
+var mongoose = require('mongoose'),
+	jwt    = require('jsonwebtoken'),
+	cfg = require("../../../../config/config.js"),
+	errorHandler = require('./errors.server.controller'),
+	User = mongoose.model('User'),
+    _ = require('lodash');
 
 /**
  * Extend user's controller
  */
-module.exports = _.extend(
+/*module.exports = _.extend(
   require('./users/users.authentication.server.controller'),
   require('./users/users.authorization.server.controller'),
   require('./users/users.password.server.controller'),
-  require('./users/users.profile.server.controller')
+  require('./users/users.profile.server.controller')*/
+  
+  /**
+ * Create a User
+ */
+exports.createUser = function(req, res) {
+	var email = req.body.email,
+        password = req.body.password,
+		name=req.body.name;
+	
+		// find a user whose email is the same as the forms email
+		// we are checking to see if the user trying to login already exists
+        User.findOne({ 'local.email' :  email }, function(err, user) {
+            // if there are any errors, return the error
+            if (err){
+				return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+			}
+            // check to see if theres already a user with that email
+            if (user) {
+                return res.status(400).send({
+					message: 'User already exists'
+				});
+            } else {
+
+				// if there is no user with that email
+                // create the user
+                var newUser            = new User();
+
+                // set the user's local credentials
+				newUser.local.name= name;
+                newUser.local.email    = email;
+                newUser.local.password = newUser.generateHash(password); // use the generateHash function in our user model
+
+				// save the user
+                newUser.save(function(err) {
+                    if (err){
+						return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+					}
+               else{
+				   res.status(201).json(newUser);
+			   }
+                });
+            }
+
+        });
+};
+
+ /**
+ * Login a User
+ */
+exports.login = function(req, res) {
+	var email = req.body.email,
+        password = req.body.password;
+// find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+        User.findOne({ 'local.email' :  email }, function(err, user) {
+            // if there are any errors, return the error before anything else
+            if (err){
+				return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+			}
+            // if no user is found, return the message
+            if (!user){
+                return res.status(400).send({
+			message: 'User Not found'
+		}); // req.flash is the way to set flashdata using connect-flash
+			}
+            // if the user is found but the password is wrong
+            if (!user.validPassword(password)){
+				return res.status(400).send({
+			message: 'Oops! Wrong password.'
+		});
+			}
+			else{
+				// all is well, return successful user
+				var token = jwt.sign(user, cfg.jwtSecret, {
+				expiresInMinutes: 5 // expires in 24 hours
+        });
+				res.json({token: token});
+			}
+            
+        });
+};
   
   /**
  * Require login routing middleware
  */
 
-);
+
--- modules/users/server/routes/users.server.routes.js
+++ modules/users/server/routes/users.server.routes.js
@@ -1,16 +1,20 @@
+/* eslint-disable */
 'use strict';
-
+var express = require("express");
+var bodyParser = require("body-parser");
+var jwt    = require('jsonwebtoken');
+var cors = require('cors');
 module.exports = function (app) {
+	app.use(cors());
   // User Routes
   var users = require('../controllers/users.server.controller');
 
   // Setting up the users profile api
-  app.route('/api/users/me').get(users.me);
-  app.route('/api/users').put(users.update);
-  app.route('/api/users/accounts').delete(users.removeOAuthProvider);
-  app.route('/api/users/password').post(users.changePassword);
-  app.route('/api/users/picture').post(users.changeProfilePicture);
+  app.route('/profile').get(users.readUserProfile);
+  app.route('/login').post(users.login);
+  app.route('/signup').post(users.createUser);
+
 
   // Finish by binding the user middleware
-  app.param('userId', users.userByID);
+  //app.param('userId', users.userByID);
 };
