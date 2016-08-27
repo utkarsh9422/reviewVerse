8e4837a312e6f5dcad5e7a2fed33d4ef86b6c18d
utkarsh9422 <UM00427537@techmahindra.com>
2016-08-27 20:07:28 +0530

    JWT error

--- modules/users/server/models/user.server.model.js
+++ modules/users/server/models/user.server.model.js
@@ -50,15 +50,13 @@ userSchema.methods.validPassword = function(password) {
 
 // Create JWT 
 userSchema.methods.generateJwt = function(){
-	var expiry = new Date();
-  expiry.setDate(expiry.getDate() + 7);
 
   return jwt.sign({
     _id: this._id,
     email: this.email,
-    name: this.name,
-    exp: parseInt(expiry.getTime() / 1000),
-  }, "MY_SECRET"); // DO NOT KEEP YOUR SECRET IN THE CODE!
+    name: this.name),
+  }, cfg.sessionSecret,
+  expiresInMinutes : 1440); // DO NOT KEEP YOUR SECRET IN THE CODE!
 };
 
 // create the model for users and expose it to our app
