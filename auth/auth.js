e3883a11eee07379f2f07a353685579ca977b196
utkarsh9422 <UM00427537@techmahindra.com>
2016-09-11 22:25:17 +0530

    reverted middlewatre

--- auth/auth.js
+++ auth/auth.js
@@ -83,30 +83,4 @@ return passport.authenticate("jwt", cfg.sessionSecret);
 }
 };
 
-/*
- |--------------------------------------------------------------------------
- | Login Required Middleware
- |--------------------------------------------------------------------------
- */
-ensureAuthenticated: function(req, res, next) {
-  if (!req.header('Authorization')) {
-    return res.status(401).send({ message: 'Please make sure your request has an Authorization header' });
-  }
-  var token = req.header('Authorization').split(' ')[1];
-
-  var payload = null;
-  try {
-    payload = jwt.decode(token, cfg.sessionSecret);
-  }
-  catch (err) {
-    return res.status(401).send({ message: err.message });
-  }
-
-  if (payload.exp <= moment().unix()) {
-    return res.status(401).send({ message: 'Token has expired' });
-  }
-  req.user = payload.sub;
-  next();
-}
-
 };
