9517c634836a3e4306b7604069e5f2f0e805abd4
utkarsh9422 <UM00427537@techmahindra.com>
2016-09-11 22:22:51 +0530

    test with middleware

--- auth/auth.js
+++ auth/auth.js
@@ -12,6 +12,8 @@ var Strategy = passportJWT.Strategy;
 var FacebookStrategy = require('passport-facebook').Strategy;  
 var TwitterStrategy = require('passport-twitter').Strategy;  
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var jwt = require('jwt-simple');
+var moment = require('moment');
 var params = {
 secretOrKey: cfg.sessionSecret,
 jwtFromRequest: ExtractJwt.fromAuthHeader()
@@ -80,4 +82,31 @@ authenticate: function() {
 return passport.authenticate("jwt", cfg.sessionSecret);
 }
 };
+
+/*
+ |--------------------------------------------------------------------------
+ | Login Required Middleware
+ |--------------------------------------------------------------------------
+ */
+ensureAuthenticated: function(req, res, next) {
+  if (!req.header('Authorization')) {
+    return res.status(401).send({ message: 'Please make sure your request has an Authorization header' });
+  }
+  var token = req.header('Authorization').split(' ')[1];
+
+  var payload = null;
+  try {
+    payload = jwt.decode(token, cfg.sessionSecret);
+  }
+  catch (err) {
+    return res.status(401).send({ message: err.message });
+  }
+
+  if (payload.exp <= moment().unix()) {
+    return res.status(401).send({ message: 'Token has expired' });
+  }
+  req.user = payload.sub;
+  next();
+}
+
 };
